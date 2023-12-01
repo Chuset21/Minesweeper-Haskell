13bff9143e19867a51a3d1d0d962733acb45729b
Miguel Arrieta <miguelarrieta02@gmail.com>
2023-12-01 17:42:28 +0000

    Implemented getting the amount of nearby mines

--- src/Board.hs
+++ src/Board.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 
-module Board (Board (..), BoardState (..), Cell (status), CellState (..), generateSeededBoard, generateBoard, prettyPrintBoard, getIf, getTotalMines) where
+module Board (Board (..), BoardState (..), Cell (status), CellState (..), generateSeededBoard, generateBoard, prettyPrintBoard, prettyPrintBoardWithNumbers, getIf) where
 
 import Control.Monad.Random
 import Data.List
@@ -32,21 +32,52 @@ prettyPrintBoard :: Board -> String
 prettyPrintBoard (Board _ boardGrid) =
   unlines $ map (concatMap debugPrintCell) $ V.toList $ fmap V.toList boardGrid
 
+prettyPrintBoardWithNumbers :: Board -> String
+prettyPrintBoardWithNumbers b =
+  unlines $ map (concatMap (\(i, c) -> if hasMine c then "[*]" else "[" ++ show (countSurroundingMines b i) ++ "]")) (getIndexedMatrix b)
+
 -- Length and width will be equal
 size :: Board -> Int
 size Board {grid = g} = V.length g
 
-getIf :: (Cell -> Bool) -> Board -> [((Int, Int), Cell)]
-getIf predicate Board {grid = g} =
-  [((i, j), cell) | (i, row) <- zip [0 ..] (V.toList g), (j, cell) <- zip [0 ..] (V.toList row), predicate cell]
+getIf :: Board -> (Cell -> Bool) -> [((Int, Int), Cell)]
+getIf Board {grid = g} predicate =
+  [((i, j), cell) | (i, row) <- V.toList $ V.indexed g, (j, cell) <- V.toList $ V.indexed row, predicate cell]
 
-getTotalMines :: Board -> Int
-getTotalMines board = length $ getIf hasMine board
+-- Get a flattened list of all the cells with indices
+getIndexedList :: Board -> [((Int, Int), Cell)]
+getIndexedList b = getIf b $ const True
 
-getCellAtIndex :: (Int, Int) -> Board -> Maybe Cell
-getCellAtIndex (i, j) Board {grid = g} = do
+-- Get a matrix of all the cells with indices
+getIndexedMatrix :: Board -> [[((Int, Int), Cell)]]
+getIndexedMatrix b = groupBy (\((x, _), _) ((y, _), _) -> x == y) (getIndexedList b) -- We want to separate the list by row, so whenever the row isn't equal, separate it onto a new list
+
+getCellAtIndexUnsafe :: Board -> (Int, Int) -> Cell
+getCellAtIndexUnsafe Board {grid = g} (i, j) = (g V.! i) V.! j
+
+getCellAtIndex :: Board -> (Int, Int) -> Maybe Cell
+getCellAtIndex Board {grid = g} (i, j) = do
   row <- g V.!? i
   row V.!? j
+
+inBounds :: Board -> (Int, Int) -> Bool
+inBounds b (i, j) =
+  let h = size b
+  in 0 <= i && 0 <= j && i < h && j < h
+
+getNeighbours :: Board -> (Int, Int) -> [((Int, Int), Cell)]
+getNeighbours b (i, j) =
+  let neighbourIndices = [(x, y) | x <- [i-1..i+1],
+                                    y <- [j-1..j+1],
+                                    not $ (x == i) && (y == j),
+                                    inBounds b (x, y) ]
+  in map (\i -> (i, getCellAtIndexUnsafe b i)) neighbourIndices
+
+getTotalMines :: [Cell] -> Int
+getTotalMines cells = length $ filter hasMine cells
+
+countSurroundingMines :: Board -> (Int, Int) -> Int
+countSurroundingMines b indices = getTotalMines $ map snd $ getNeighbours b indices
 
 generateSeededBoard :: RandomGen g => g -> Int -> Int -> (Board, g)
 generateSeededBoard gen size numBombs =
--- src/Main.hs
+++ src/Main.hs
@@ -9,25 +9,18 @@ import Board
 import System.Random (newStdGen, RandomGen)
 import Control.Monad.RWS (MonadState(put))
 
-repeatProcess :: (Eq n, Num n, RandomGen g) => g -> n -> Int -> Int -> IO ()
-repeatProcess _ 0 _ _ = return ()
-repeatProcess gen n size numBombs = do
-  let (board, newGen) = generateSeededBoard gen size numBombs
-  when (getTotalMines board /= numBombs) $ print "Error!!!!!!!!!!!!!!!!"
-  repeatProcess newGen (n - 1) size numBombs
-
 main :: IO ()
 main = do
   gen <- newStdGen
   -- repeatProcess gen 100 25 99
   let (b, _) = generateSeededBoard gen 10 45
-  putStrLn $ prettyPrintBoard b
+  putStrLn $ prettyPrintBoardWithNumbers b
   b <- generateBoard 10 45
-  putStrLn $ prettyPrintBoard b
+  putStrLn $ prettyPrintBoardWithNumbers b
   b <- generateBoard 10 45
-  putStrLn $ prettyPrintBoard b
+  putStrLn $ prettyPrintBoardWithNumbers b
   b <- generateBoard 10 45
-  putStrLn $ prettyPrintBoard b
+  putStrLn $ prettyPrintBoardWithNumbers b
 
   startGUI defaultConfig setup
 
