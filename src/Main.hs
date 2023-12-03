ad66227b2b4f988bc4cfd529836f21cb44f7bf96
Miguel Arrieta <miguelarrieta02@gmail.com>
2023-12-03 09:30:17 +0000

    Created button for changing modes

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,9 @@ import Data.IORef (IORef, modifyIORef, newIORef, readIORef, writeIORef)
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core hiding (grid)
 import System.Random (RandomGen, newStdGen)
+import Data.Bits
+
+data Mode = Flagging | Mining deriving (Eq, Show)
 
 path = "static"
 
@@ -36,13 +39,25 @@ setup window = do
   boardRef <- liftIO $ newIORef =<< generateBoard 10 10
   board <- liftIO $ readIORef boardRef
 
+  modeRef <- liftIO $ newIORef Mining
+  let modeButton = do
+        mode <- liftIO $ readIORef modeRef
+        btn <-
+          UI.button # set text (btnText mode)
+        on UI.click btn $ \_ -> do
+          liftIO $ modifyIORef modeRef (\s -> if s == Flagging then Mining else Flagging)
+          mode <- liftIO $ readIORef modeRef
+          element btn # set text (btnText mode)
+        return btn
+        where btnText m = "Current mode: " ++ show m
+
   let updateVisuals board action = do
         element <- getElementById window "board"
         case element of
           Nothing -> pure ()
           Just x -> do
             pure x # set children [] -- Clear existing children
-            let buttons = mkTable action $ grid $ getBoardVisuals board
+            let buttons = mkTable action modeRef $ grid $ getBoardVisuals board
             void $ pure x #+ [buttons]
 
   let revealAndUpdateBoard :: (Int, Int) -> (Board -> (Int, Int) -> Board) -> UI ()
@@ -54,32 +69,35 @@ setup window = do
         liftIO $ writeIORef boardRef updatedBoard
 
         updateVisuals updatedBoard revealAndUpdateBoard -- This is bugged and hangs the program
-  let buttons = mkTable revealAndUpdateBoard $ grid $ getBoardVisuals board
-
-  liftIO $ putStrLn $ prettyPrintBoardWithNumbers board
+  let buttons = mkTable revealAndUpdateBoard modeRef $ grid $ getBoardVisuals board
 
   -- Add buttons to the body of the HTML document
-  void $ getBody window #+ [buttons]
+  void $ getBody window #+ [modeButton, buttons]
 
-mkCell actionOnClick (indices, s) = do
+mkCell actionOnClick modeRef (indices, s) = do
   btn <-
     UI.td
       # set style [("width", "50px"), ("height", "50px"), ("margin", "0px"), ("padding", "0px"), ("border", "1px solid black")]
       #+ [ UI.img # set style [("width", "100%"), ("height", "100%"), ("margin", "0px"), ("padding", "0px"), ("display", "block")]
              # set UI.src (imageURL s)
          ]
-  on UI.click btn $ \_ -> actionOnClick indices revealCell
-  on UI.contextmenu btn $ \_ -> actionOnClick indices toggleFlag
+  on UI.click btn $ \_ -> do
+    mode <- liftIO $ readIORef modeRef
+    actionOnClick indices (boardAction mode True)
+  on UI.contextmenu btn $ \_ -> do 
+    mode <- liftIO $ readIORef modeRef
+    actionOnClick indices (boardAction mode False)
 
   return btn
+  where boardAction m isClick = if (m == Mining) /= isClick then toggleFlag else revealCell
 
-mkRow actionOnClick cols =
+mkRow actionOnClick modeRef cols =
   UI.tr
     # set style [("margin", "0px"), ("padding", "0px"), ("border", "1px solid black")]
-    #+ map (mkCell actionOnClick) cols
+    #+ map (mkCell actionOnClick modeRef) cols
 
-mkTable actionOnClick rows =
+mkTable actionOnClick modeRef rows =
   UI.table
     # set UI.id_ "board"
     # set style [("border-spacing", "0px"), ("border-collapse", "collapse"), ("cellspacing", "0px"), ("margin", "0px"), ("padding", "0px"), ("border", "1px solid black")]
-    #+ map (mkRow actionOnClick) rows
+    #+ map (mkRow actionOnClick modeRef) rows
